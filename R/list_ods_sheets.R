8c99ffe58a17652d6ef63133601633cdcd7ebc5c
pbrohan <peter.brohan@gmail.com>
2023-06-16 16:47:25 +0100

    Initial commit

---
+++ R/list_ods_sheets.R
@@ -0,0 +1,10 @@
+#' List all sheets in an ODS File
+#' 
+#' List all shgeets in an ods file.
+#' 
+#' @param path Path to the ods file
+#' @return A character vector of sheet names.
+#' @export
+list_ods_sheets <- function(path) {
+    return(c("one"))
+}
